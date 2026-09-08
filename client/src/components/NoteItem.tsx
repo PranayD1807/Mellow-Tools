82a265150d579f3773c3193373ae3fb91b66f7e5
ImperialVishesh <vishrshanand1234@gmail.com>
2026-09-08 12:56:25 +0530

    fix: ESLint error

--- client/src/components/NoteItem.tsx
+++ client/src/components/NoteItem.tsx
@@ -22,7 +22,7 @@ import { TextNote } from "@/models/TextNote";
 import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from "./ui/menu";
 import { toast } from "react-toastify";
 import LinkifyModule from "react-linkify";
-const Linkify = (LinkifyModule as any).default || LinkifyModule;
+const Linkify = (LinkifyModule as unknown as { default?: typeof LinkifyModule }).default || LinkifyModule;
 import { HiOutlineDotsVertical } from "react-icons/hi";
 
 interface NoteItemProps {
